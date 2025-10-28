e81d5caf80eec3c956762799befcabd4e8067f74
Ryota Shioya <shioya@ci.i.u-tokyo.ac.jp>
2025-10-28 17:11:38 +0900

    chore: update version number to 0.0.4

--- src/ui_parts.tsx
+++ src/ui_parts.tsx
@@ -328,7 +328,7 @@ const VersionDialog = (props: {store: Store;}) => {
         <Modal.Header closeButton>
             <Modal.Title>Version Information</Modal.Title>
         </Modal.Header>  
-        <Modal.Body>Meno Version 0.0.3</Modal.Body>             
+        <Modal.Body>Meno Version 0.0.4</Modal.Body>             
         </Modal>
     );
 };
